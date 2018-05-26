d42d31316ab9e541239733e1848438f0f6596770
Andrei Ion <andrei.ion@stud.acs.upb.ro>
2018-05-26 03:52:21 +0300

    Add bashrc with tmux aliases and functions from Michael Kerrisk

--- bash.sh
+++ bash.sh
@@ -1,3 +1,19 @@
 
+#Contains the process ID of the most recently executed background pipeline
 echo $!
+
 rm $(!!)
+
+##########
+echo 'test' > /tmp/a
+cat !$ #output '/tmp/a'
+
+
+#########
+echo $$ #prints current sell process
+
+# delete every file that has one char
+rm a/?
+
+# delete every file that has two chars
+rm a/??
